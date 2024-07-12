0af3cccd59607114eb028fc3889ecef5410b579b
Luphia Chang <luphia@mermer.cc>
2024-07-12 17:06:13 +0800

    Update miner.sh

--- shell/miner.sh
+++ shell/miner.sh
@@ -9,8 +9,8 @@ selectedAddress=${addresses[ $RANDOM % ${#addresses[@]} ]}
 
 echo ${selectedAddress}
 
-# 刪除相關資料
+# 刪除舊資料
 rm /workspace/isuncoin-miner/iSunCoin/chaindata/ /workspace/isuncoin-miner/iSunCoin/ethash/ /workspace/isuncoin-miner/iSunCoin/jwtsecret /workspace/isuncoin-miner/iSunCoin/LOCK /workspace/isuncoin-miner/iSunCoin/transactions.rlp /workspace/isuncoin-miner/iSunCoin/triecache/ -rf
 
-# 啟動新的 isuncoin 進程
+# 啟動 isuncoin 挖礦
 screen -dmS isuncoin_miner isuncoin --datadir /workspace/isuncoin-miner --mine --miner.threads=1 --miner.etherbase ${selectedAddress} --port 30303 --authrpc.port 8553
