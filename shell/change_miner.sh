7765ad66b8d8d26d688964d1bc75d8b92b596a7c
Luphia Chang <luphia@mermer.cc>
2024-07-12 17:07:12 +0800

    Update change_miner.sh

--- shell/change_miner.sh
+++ shell/change_miner.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# 定義地址陣列
 addresses=("0xCAFECA05eB2686e2D7e78449F35d8F6D2Faee174")
 selectedAddress=${addresses[ $RANDOM % ${#addresses[@]} ]}
+
+# 替換挖礦獎勵地址
 isuncoin --datadir /workspace/isuncoin-miner/ attach <<< "miner.setEtherbase(\"$selectedAddress\")"
