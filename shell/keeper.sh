4dc8c1f4b03c28ed6d03c02bde21596e928a27d7
Luphia Chang <luphia@mermer.cc>
2024-07-12 18:41:07 +0800

    Update keeper.sh

--- shell/keeper.sh
+++ shell/keeper.sh
@@ -4,14 +4,8 @@
 # 檢查並關閉已經運行的 isuncoin-keeper
 screen -X -S isuncoin-keeper quit
 
-# 定義地址陣列
-addresses=("0xCAFECA05eB2686e2D7e78449F35d8F6D2Faee174")
-selectedAddress=${addresses[ $RANDOM % ${#addresses[@]} ]}
-
-echo ${selectedAddress}
-
 # 刪除舊資料
 rm /workspace/isuncoin-keeper/iSunCoin/chaindata/ /workspace/isuncoin-keeper/iSunCoin/ethash/ /workspace/isuncoin-keeper/iSunCoin/jwtsecret /workspace/isuncoin-keeper/iSunCoin/LOCK /workspace/isuncoin-keeper/iSunCoin/transactions.rlp /workspace/isuncoin-keeper/iSunCoin/triecache/ -rf
 
 # 啟動 isuncoin 同步數據
-screen -dmS isuncoin-keeper isuncoin --datadir /workspace/isuncoin-keeper --miner.etherbase $selectedAddress
+screen -dmS isuncoin-keeper isuncoin --datadir /workspace/isuncoin-keeper --port 30303 --authrpc.port 8553
