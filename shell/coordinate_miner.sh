8c728c0ba44929da3ef8fd8f02d3e45aa0cf5935
Luphia Chang <luphia@cafeca.io>
2024-07-12 22:08:31 +0800

    enhance coordinate logic

--- shell/coordinate_miner.sh
+++ shell/coordinate_miner.sh
@@ -32,39 +32,40 @@ get_remote_block_hash() {
 # 取得目前連線節點數量
 peer_count=$(get_peer_count)
 
-if [ "$peer_count" -lt 1 ]; then
+if [ "$peer_count" -lt 1 -o -z "$peer_count" ]; then
+    echo "重新初始化區塊鏈網路連線"
+
     # 關閉已經運行的 isuncoin-miner
     screen -X -S isuncoin-miner quit
-    echo "重新初始化區塊鏈網路連線"
-fi
 
-# 取得本地最新區塊號碼
-local_latest_block_number=$(get_local_latest_block_number)
-
-# 目標區塊
-target_block_number=$((local_latest_block_number - 10))
-
-# 檢查目標區塊號碼是否大於 0
-if [ "$target_block_number" -gt 0 ]; then
-
-    # 取得本地目標區塊的 hash
-    local_block_hash=$(get_local_block_hash $target_block_number)
-
-    # 取得遠端目標區塊的 hash
-    remote_block_hash=$(get_remote_block_hash $target_block_number "https://isuncoin.baifa.io")
-
-    # 比較本地和遠端的 blockhash
-    echo "檢驗區塊: $target_block_number"
-    echo "本地資料: $local_block_hash"
-    echo "遠端資料: $remote_block_hash"
-    if [ "$local_block_hash" != "$remote_block_hash" ]; then
-        "$BASEDIR/kind_miner.sh"
-    else
-        echo "本地和遠端的區塊 blockhash 一致"
-    fi
-elif [ -z "$local_latest_block_number" ]; then
-    echo "嘗試啟動挖礦"
+    # 重新啟動挖礦    
     "$BASEDIR/kind_miner.sh"
 else
-    echo "區塊鏈尚在同步中"
+    # 取得本地最新區塊號碼
+    local_latest_block_number=$(get_local_latest_block_number)
+
+    # 目標區塊
+    target_block_number=$((local_latest_block_number - 10))
+
+    # 檢查目標區塊號碼是否大於 0
+    if [ "$target_block_number" -gt 0 ]; then
+
+        # 取得本地目標區塊的 hash
+        local_block_hash=$(get_local_block_hash $target_block_number)
+
+        # 取得遠端目標區塊的 hash
+        remote_block_hash=$(get_remote_block_hash $target_block_number "https://isuncoin.baifa.io")
+
+        # 比較本地和遠端的 blockhash
+        echo "檢驗區塊: $target_block_number"
+        echo "本地資料: $local_block_hash"
+        echo "遠端資料: $remote_block_hash"
+        if [ "$local_block_hash" != "$remote_block_hash" ]; then
+            "$BASEDIR/kind_miner.sh"
+        else
+            echo "本地和遠端的區塊 blockhash 一致"
+        fi
+    else
+        echo "區塊鏈尚在同步中"
+    fi
 fi
